0e7c5c93b06d3dc2660cb0fb061255195f5aa9bc
Aecio Levy <aeciolevy@gmail.com>
2017-09-20 02:06:07 -0300

    Updating user-webtask.js

--- user-webtask.js
+++ user-webtask.js
@@ -67,7 +67,7 @@ app.get('/users', function(req, res){
       res.writeHead(400, { 'Content-Type': 'application/json'});
       res.end(JSON.stringify(RESPONSE.ERROR));
     } else {
-      res.writeHead(200, { 'Content-Type': 'application/json'});
+      // res.writeHead(200, { 'Content-Type': 'application/json'});
       res.json(data.users);
     }
   });
