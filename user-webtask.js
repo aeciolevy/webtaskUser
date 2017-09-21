b3094d864c957f6c05328b6c523ee334428d847d
Aecio Levy <aeciolevy@gmail.com>
2017-09-21 12:09:51 -0300

    Updating user-webtask.js

--- user-webtask.js
+++ user-webtask.js
@@ -147,8 +147,7 @@ app.delete('/user', function(req, res) {
             delete data.usergroup.byId[String(index)];
           }
         });
-        data.usergroup.test = _.keys(Number(data.usergroup.byId));
-        
+        data.usergroup.test = _.keys(data.usergroup.byId).map( elem => Number(elem));
          
         req.webtaskContext.storage.set(data, function(err){
           if(err){
